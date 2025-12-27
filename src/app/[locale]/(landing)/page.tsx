fb70b56d9ac4016b4a6ec4820118632b9d4d482c
neco3coffee <neco3coffee@gmail.com>
2025-12-27 22:20:43 +0900

    Landing Page(サービスの価値,羅針盤)の骨格作成

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -1,6 +1,7 @@
-import styles from "./page.module.scss";
+import feature1s from "./page.module.scss";
 import { getTranslations } from "next-intl/server";
 import { setRequestLocale } from "next-intl/server";
+import styles from "./page.module.scss";
 
 export default async function Page({
   params,
@@ -14,7 +15,40 @@ export default async function Page({
 
   return (
     <main className={styles.main}>
-      <h1>{t("title")}</h1>
+      <section className={styles.section}>
+        <span>HeroSection</span>
+        <h3>{t("HeroSection.title")}</h3>
+        <p>{t("HeroSection.description")}</p>
+      </section>
+      <section className={styles.section}>
+        <span>FeatureSection</span>
+        <h3>{t("FeatureSection.feature1.title")}</h3>
+        <p>{t("FeatureSection.feature1.description")}</p>
+        <h3>{t("FeatureSection.feature2.title")}</h3>
+        <p>{t("FeatureSection.feature2.description")}</p>
+        <h3>{t("FeatureSection.feature3.title")}</h3>
+        <p>{t("FeatureSection.feature3.description")}</p>
+      </section>
+      <section className={styles.section}>
+        <span>SocialProofSection</span>
+        <h3>{t("SocialProofSection.title")}</h3>
+      </section>
+      <section className={styles.section}>
+        <span>FAQSection</span>
+        <h3>{t("FAQSection.question1.question")}</h3>
+        <p>{t("FAQSection.question1.answer")}</p>
+        <h3>{t("FAQSection.question2.question")}</h3>
+        <p>{t("FAQSection.question2.answer")}</p>
+        <h3>{t("FAQSection.question3.question")}</h3>
+        <p>{t("FAQSection.question3.answer")}</p>
+      </section>
+      <section className={styles.section}>
+        <span>CallToActionSection</span>
+        <h3>{t("CallToActionSection.title")}</h3>
+        <p>{t("CallToActionSection.description")}</p>
+        <button>{t("CallToActionSection.buttonText")}</button>
+      </section>
+      
     </main>
   );
 }
