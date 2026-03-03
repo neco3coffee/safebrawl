96721926e0065a5cb3a90e59eaa15078c645bf19
neco3coffee <neco3coffee@gmail.com>
2026-03-03 11:55:36 +0900

    add: Brawlerのtypeを2026/2のupdateに対応したものに修正

--- packages/shared/brawl-stars-api/types.ts
+++ packages/shared/brawl-stars-api/types.ts
@@ -36,11 +36,29 @@ export interface Brawler {
   rank: number
   trophies: number
   highestTrophies: number
+  prestigeLevel: number
+  currentWinStreak: number
   maxWinStreak: number
+  skin: Skin
+  gadgets: Gadget[] | []
   gears: Gear[] | []
   starPowers: StarPower[] | []
-  gadgets: Gadget[] | []
-  currentWinStreak: number
+  hyperCharges: HyperCharge[] | []
+  buffies: {
+    gadget: boolean
+    starPower: boolean
+    hyperCharge: boolean
+  }
+}
+
+export interface HyperCharge {
+  id: number
+  name: string
+}
+
+export interface Skin {
+  id: number
+  name: string
 }
 
 export interface Gear {
